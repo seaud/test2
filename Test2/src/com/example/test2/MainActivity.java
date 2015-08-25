4f4ce3b22a1c439454e2ae739ba327b0a8ac32d8
seaud <seaud@wowventures.co.kr>
2015-08-25 16:17:37 +0900

    asdasdas

--- Test2/src/com/example/test2/MainActivity.java
+++ Test2/src/com/example/test2/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends ActionBarActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		int a = 10;
+		int b = 10;
 		return true;
 	}
 
